693cc2fbbe0b667fe09f2ef5bea4fb495015e718
siva kurapati <siva kurapati@192.168.0.5>
2017-04-16 16:39:53 +0530

    Refactoring for the user page

--- OSCWeb/WebContent/resource/js/controller/HeaderController.js
+++ OSCWeb/WebContent/resource/js/controller/HeaderController.js
@@ -1,7 +1,10 @@
 'use strict';
 
 App.controller('HeaderController', ['$scope','$http','$rootScope','$state', function($scope,$http,$rootScope,$state) {
-
+	if(!$rootScope.rsAddedCartItemList){
+	$rootScope.rsAddedCartItemList = [];
+	}
+	
 	$scope.getAllCategoriesWithSubCategory = function(){
 		if(!$rootScope.navigationDataList){
   		var response = $http.get(constants.localhost_port+constants.service_context+"/SubCategoryController/allCategoriesWithSubCategory");
@@ -15,6 +18,21 @@ App.controller('HeaderController', ['$scope','$http','$rootScope','$state', func
 		}
   	}
 	
+	 //find the sum of all the items
+    $scope.totalCost = function(){
+  	  if($rootScope.rsAddedCartItemList){
+  		  $scope.totalAmount = 0;
+  	  angular.forEach($rootScope.rsAddedCartItemList, function(obj, key) {
+  		$scope.totalAmount = parseFloat($scope.totalAmount)+parseFloat(obj.total);
+		}); 
+  	$scope.totalAmount = Math.round($scope.totalAmount * 100) / 100;
+  	  return $scope.totalAmount;
+  	  }
+    };
 	
+    $scope.viewAllItems = function(subCategoryId){
+		$state.go("view_all_items",{id:subCategoryId});
+	}
+    
 	$scope.getAllCategoriesWithSubCategory();
 }]);
--- OSCWeb/WebContent/resource/js/controller/ItemController.js
+++ OSCWeb/WebContent/resource/js/controller/ItemController.js
@@ -9,13 +9,17 @@ App.controller('ItemController', ['$scope','$http','$rootScope','$state','$state
 		var response = $http.get(constants.localhost_port+constants.service_context+"/ItemController/getItemById/"+$stateParams.id);
   		response.success(function(data) {
   			$scope.itemObj = data;
+  			$scope.customerItem = data;
+  			
   			if($scope.itemObj && $scope.itemObj.discount && $scope.itemObj.discount != 0){
   				$scope.itemObj.mrp = $scope.itemObj.mrp - ($scope.itemObj.mrp * $scope.itemObj.discount) / 100;
   				$scope.itemObj.mrp = parseFloat($scope.itemObj.mrp).toFixed(2);
+  				$scope.customerItem.mrp =$scope.itemObj.mrp;
   				$scope.customerItem.total = $scope.itemObj.mrp;
   				if($scope.itemObj.minQuantityToPurchase){
   					$scope.itemObj.mrp = parseFloat($scope.itemObj.mrp / $scope.itemObj.minQuantityToPurchase).toFixed(2);//This is for each quantity 
   				}
+  					
   				
   				
   				$scope.customerItem.quantity  = $scope.itemObj.minQuantityToPurchase;
@@ -38,6 +42,33 @@ App.controller('ItemController', ['$scope','$http','$rootScope','$state','$state
 		}else{
 			$scope.errorMsg = true;
 		}
+	}
+
+	$scope.addToCartPerformAction = function(){
+		var isItemExists = false;
+		if($rootScope.rsAddedCartItemList){
+		angular.forEach($rootScope.rsAddedCartItemList, function(obj, key) {
+			if(obj.id == $scope.customerItem.id)
+			{
+				isItemExists = true;
+			}
+		});
+		if(!isItemExists){
+			$rootScope.rsAddedCartItemList.push($scope.customerItem);
+		}
+		}
+		$('#addToCartPopupId').modal('hide');
+	}
+	
+	
+	
+
+	
+	$scope.addToCartAction = function(){
+		angular.element('#addToCartPopup').trigger('click');
+	}
+	$scope.closeDialog = function(){
+		$('#addToCartPopupId').modal('hide');
 	}
 	
 	$scope.getItemInfo();
