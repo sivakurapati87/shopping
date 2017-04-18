f27ec7e445cceb12a8244b4b4fc60f3df37fbc32
siva kurapati <siva kurapati@192.168.0.4>
2017-04-18 09:07:30 +0530

    Refactoring for home changes

--- OSCWeb/WebContent/resource/js/controller/HeaderController.js
+++ OSCWeb/WebContent/resource/js/controller/HeaderController.js
@@ -9,7 +9,19 @@ App.controller('HeaderController', ['$scope','$http','$rootScope','$state', func
 		if(!$rootScope.navigationDataList){
   		var response = $http.get(constants.localhost_port+constants.service_context+"/SubCategoryController/allCategoriesWithSubCategory");
   		response.success(function(data) {
+  			if(data){
+  				$rootScope.searchDataList = [];
   			$rootScope.navigationDataList = data;
+  			$.each(data, function (i, val) {
+  			  $.each(val, function(innerKey, innerValue) {
+  				  for(var j=0;j<innerValue.length;j++){
+  					  var obj = innerValue[j];
+  					  obj.name = obj.name+' in '+ obj.categoryName;
+  					$rootScope.searchDataList.push(obj);
+  				  }
+  			  });
+  			});
+  			}
   		});
   		response.error(function() {
         	  console.error('Could not Perform well');
@@ -17,6 +29,18 @@ App.controller('HeaderController', ['$scope','$http','$rootScope','$state', func
           });
 		}
   	}
+	
+	  //This function is to get the selected value from auto population
+	  $scope.selectedCategoryDivisionNameAction = function(selected) {
+	      if (selected) {
+//	    	  $rootScope.transactionData.customerId = selected.title;
+//	    	  $scope.getCustomerInfoById();
+	      	$scope.subCategoryId = selected.originalObject.id;
+	      } else {
+	        console.log('cleared');
+	      }
+	    };
+
 	
 	 //find the sum of all the items
     $scope.totalCost = function(){
@@ -31,7 +55,9 @@ App.controller('HeaderController', ['$scope','$http','$rootScope','$state', func
     };
 	
     $scope.viewAllItems = function(subCategoryId){
+    	if(subCategoryId){
 		$state.go("view_all_items",{id:subCategoryId});
+    	}
 	}
     
 	$scope.getAllCategoriesWithSubCategory();
